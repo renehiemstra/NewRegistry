12be918ce25e8e02cae8a15179f5f772d07957c1
rrhiemstar <rrhiemstar@gmail.com>
2024-02-21 11:09:43 +0100

    Initialized new registry.

---
+++ Registry.lua
@@ -0,0 +1,8 @@
+Registry = {
+    name = "TestRegistry",
+    uuid = "0e2c046e-8562-44b9-b1e6-1b9e8e4da0c0",
+    url  = "git@github.com:renehiemstra/NewRegistry.git",
+    description = "TestRegistry local package registry",
+    packages = {}
+}
+return Registry
